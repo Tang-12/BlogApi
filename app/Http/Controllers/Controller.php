04df2c3e4eb11e546e88edf6ba8e859945ed8475
tangjun <1953248054@qq.com>
2022-07-13 22:17:45 +0800

    修改了响应输出方法

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,11 +13,11 @@ class Controller extends BaseController
 
   public function _success($msg = '', $data = [])
   {
-    echo json_encode(['code' => 200, 'msg' => $msg, 'data' => $data], JSON_UNESCAPED_UNICODE); exit;
+    echo json_encode(['code' => 200, 'msg' => $msg, 'data' => $data], JSON_UNESCAPED_UNICODE);
   }
 
   public function _error($msg = '', int $code = 400, $data = array())
   {
-    echo json_encode(['code'=>$code, 'msg'=>$msg, 'data'=>$data], JSON_UNESCAPED_UNICODE); exit();
+    echo json_encode(['code'=>$code, 'msg'=>$msg, 'data'=>$data], JSON_UNESCAPED_UNICODE);
   }
 }
